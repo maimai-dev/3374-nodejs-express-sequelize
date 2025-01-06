2be579d75bae2b43565a3fd62631fd6e7de1e502
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2025-01-06 17:01:47 -0300

    commit 06/01

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -1,11 +1,12 @@
-class PessoaController {
-    static async pegaTodas (req, res) {
-        try {
-            //acessar o modelo
-        } catch (erro){
-            //tratar erro
-        }
-    }
+const Controller = require('./Controller.js');
+const PessoaServices = require('../services/PessoaServices.js');
+
+const pessoaServices = new PessoaServices();
+
+class PessoaController extends Controller{
+  constructor(){
+    super(pessoaServices);
+  }
 }
 
 module.exports = PessoaController;
