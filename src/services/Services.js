42b0cdf106f2f6473f5dbe283d49f3abed591519
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2025-06-03 18:47:23 -0300

    feat: adicionado novas rotas para matriculas e cursos

--- src/services/Services.js
+++ src/services/Services.js
@@ -5,8 +5,8 @@ class Services {
     this.model = nomeDoModel;
   }
 
-  async pegaTodosOsRegistros () {
-    return dataSource[this.model].findAll();
+  async pegaTodosOsRegistros (where = {}) {
+    return dataSource[this.model].findAll({ where: {...where}});
   }
 
   async pegaRegistrosPorEscopo(escopo){
@@ -19,6 +19,14 @@ class Services {
   
   async pegaUmRegistro(where) {
     return dataSource[this.model].findOne({where: {...where}});
+  }
+
+  async pegaEContaRegistros(where){
+    return dataSource[this.model].findAndCountAll({
+      where: {...where},
+      limit: 2,
+      order: [['id', 'DESC']]
+    });
   }
 
   async criaRegistro(dadosDoRegistro) {
