8a1f0ce854b0138ab6c766fbbf66d2e8955af485
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2025-06-04 13:50:23 -0300

    FEAT: Adicionado pegar cursos lotados

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -1,5 +1,6 @@
 const Controller = require('./Controller.js');
 const MatriculaServices = require('../services/MatriculaServices.js');
+const Sequelize = require('sequelize');
 
 const matriculaServices = new MatriculaServices();
 
@@ -11,10 +12,31 @@ class PessoaController extends Controller{
       const { estudante_id } = req.params;
       try {
         const listaMatriculasPorEstudante = await matriculaServices.pegaEContaRegistros({
-          estudante_id: Number(estudante_id),
-          status: 'matriculado'
+          where: {
+              estudante_id: Number(estudante_id),
+              status: 'matriculado'
+          },
+          limit: 2,
+          order: [['id', 'DESC']]
         });
         return res.status(200).json(listaMatriculasPorEstudante);
+      } catch (erro) {
+        return res.status(500).json({ erro : erro.message});
+      }
+    }
+
+    async pegaCursosLotados(req, res){
+      const lotacaoCurso = 2;
+      try {
+        const cursosLotados = await matriculaServices.pegaEContaRegistros({
+          where: {
+            status: 'matriculado'
+          },
+          attributes: ['curso_id'],
+          group: ['curso_id'],
+          having: Sequelize.literal(`count(curso_id) >= ${lotacaoCurso}`)
+        });
+        return res.status(200).json(cursosLotados.count);
       } catch (erro) {
         return res.status(500).json({ erro : erro.message});
       }
